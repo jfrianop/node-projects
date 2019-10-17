b4eb7cbd04675b35af3fb7e08b03545333ab58a1
jfrianop <jfrianop@gmail.com>
2019-10-16 19:22:37 -0500

    Revert "Added basic authentication"
    This reverts commit c1a47a0e33900c99ced141a632c5a60af7a079de.

--- public/app.js
+++ public/app.js
@@ -8,159 +8,25 @@ const appendProject = project => {
   `);
 }
 
-//Get a cookie by name
-const getCookie = (cname) => {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-
-//Delete a cookie by name
-const deleteCookie = (cname) => {
-  document.cookie = cname + '=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-};
-
 // cargar los proyectos
-const loadProjects = () => {
-  $.ajax({
-    url: "/projects"
-  }).done(projects => {
-    projects.forEach(project => appendProject(project));
-  }).fail(err => {
-    // si es un err.status 401 mostar el formulario de login
-    console.log("Error", err)
-  });
-}
-
-//On Document Ready
-$(document).ready(() => {
-  $("#register").hide();
-  if (getCookie("express:sess").length == 0) {
-    $("#login").show();
-    $("#projects").hide();
-
-  } else {
-    $("#login").hide();
-    $("#projects").show();
-    $("tbody").html("");
-    loadProjects();
-  }
-})
-
-//Logout
-$(".logout").on("click", () => {
-  deleteCookie("express:sess");
-  deleteCookie("express:sess.sig");
-  $("#login").show();
-  $("#projects").hide();
-})
-
-//Open Registration
-$(".show-register").on("click", () => {
-  $("#register").show();
-  $("#login").hide();
-})
-
-//Open Login
-$(".show-login").on("click", () => {
-  $("#login").show();
-  $("#register").hide();
-})
-
-//Register User Submit
-$(".register").on("submit", e => {
-  e.preventDefault();
-
-  // limpiar los errores
-  $("span.error").remove();
-
-  const email = $(".register #email").val();
-  const password = $(".register #password").val();
-
-  $.ajax({
-    method: "POST",
-    url: "/register",
-    contentType: "application/json",
-    data: JSON.stringify({ email, password })
-  }).done(() => {
-    $("#login").show();
-    $("#register").hide();
-    $(".register #email").val("");
-    $(".register #password").val("");
-  }).fail(err => {
-    if (err.status === 422) {
-      const errors = err.responseJSON.errors;
-      if (errors.email) {
-        $(`<span class="error">${errors.email.message}</span>`).insertAfter(".register #email");
-      }
-      if (errors.password) {
-        $(`<span class="error">${errors.password.message}</span>`).insertAfter(".register #password");
-      }
-    } else {
-      console.log("Error: ", err);
-    }
-  });
+$.ajax({
+  url: "/projects"
+}).done(projects => {
+  projects.forEach(project => appendProject(project));
+}).fail(err => {
+  // si es un err.status 401 mostar el formulario de login
+  console.log("Error", err)
 });
 
-//Login Submit
-$(".login").on("submit", e => {
+//
+$("form").on("submit", e => {
   e.preventDefault();
 
   // limpiar los errores
   $("span.error").remove();
 
-  const email = $(".login #email").val();
-  const password = $(".login #password").val();
-
-  $.ajax({
-    method: "POST",
-    url: "/login",
-    contentType: "application/json",
-    data: JSON.stringify({ email, password })
-  }).done(login => {
-    appendProject(login);
-    $("#login").hide();
-    $("#projects").show();
-    $("tbody").html("");
-    loadProjects();
-    $(".login #email").val("");
-    $(".login #password").val("");
-  }).fail(err => {
-    if (err.status === 422) {
-      const errors = err.responseJSON.errors;
-      if (errors.email) {
-        $(`<span class="error">${errors.email.message}</span>`).insertAfter(".login #email");
-      }
-      if (errors.password) {
-        $(`<span class="error">${errors.password.message}</span>`).insertAfter(".login #password");
-      }
-    } else if (err.status === 401) {
-      alert(err.responseJSON.error)
-    } else {
-      console.log("Error: ", err);
-    }
-  });
-});
-
-//Project Submit
-$(".projects").on("submit", e => {
-  e.preventDefault();
-
-  // limpiar los errores
-  $("span.error").remove();
-
-  const name = $(".projects #name").val();
-  const description = $(".projects #description").val();
+  const name = $("#name").val();
+  const description = $("#description").val();
 
   $.ajax({
     method: "POST",
@@ -176,7 +42,7 @@ $(".projects").on("submit", e => {
     if (err.status === 422) {
       const errors = err.responseJSON.errors;
       if (errors.name) {
-        $(`<span class="error">${errors.name.message}</span>`).insertAfter(".projects #name");
+        $(`<span class="error">${errors.name.message}</span>`).insertAfter("#name");
       }
     } else {
       console.log("Error: ", err);
--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cookieSession = require('cookie-session')
 
 const app = express();
 
 //Connect to database
-mongoose.connect('mongodb+srv://admin:admin@test-5s8sm.mongodb.net/IssuesProject?retryWrites=true&w=majority',
-  { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://127.0.0.1:27017/projects', { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Project Schema
 const projectSchema = mongoose.Schema({
@@ -19,50 +17,19 @@ const projectSchema = mongoose.Schema({
 });
 
 //ProjectModel
-const Project = mongoose.model("Projects", projectSchema, "Projects");
+const Project = mongoose.model("Project", projectSchema);
 
-//User Schema
-const userSchema = mongoose.Schema({
-  email: {
-    type: String,
-    required: [true, "is required"]
-  },
-  password: {
-    type: String,
-    required: [true, "is required"]
-  },
-});
-
-//UserModel
-const User = mongoose.model("User", userSchema);
-
-//-----------------Middlewares---------------------------------------------
-//Static folder
+//Middlewares
 app.use(express.static("public"));
-
-//JSON parser
 app.use(express.json());
-
-//Cookie session
-app.use(cookieSession({
-  secret: "da344483bb83f6f7f89b358cd1440e53",
-  // Cookie Options
-  httpOnly: false,
-  maxAge: 30 * 60 * 1000 // 30 min
-}))
-
 
 //GET projects
 app.get("/projects", async (req, res, next) => {
-  if (req.session.userId) {
-    try {
-      const projects = await Project.find();
-      res.json(projects);
-    } catch (err) {
-      next(err);
-    }
-  } else {
-    res.status(401).json({ error: "Not Authenticated" })
+  try {
+    const projects = await Project.find();
+    res.json(projects);
+  } catch (err) {
+    next(err);
   }
 });
 
@@ -82,44 +49,6 @@ app.post("/projects", async (req, res, next) => {
     }
   }
 });
-
-//POST register (User registration)
-app.post("/register", async (req, res, next) => {
-  const newUser = req.body;
-
-  try {
-    const user = await User.create(newUser)
-    res.json(user);
-  } catch (err) {
-    if (err.name === "ValidationError") {
-      res.status(422).json({ errors: err.errors });
-    } else {
-      next(err);
-    }
-  }
-});
-
-//POST register (User registration)
-app.post("/login", async (req, res, next) => {
-  const newLogin = req.body;
-
-  try {
-    const user = await User.findOne({ email: newLogin.email, password: newLogin.password });
-    if (user) {
-      req.session.userId = user._id;
-      res.json(user);
-    } else {
-      res.status(401).json({ error: "Combinación de correo/contraseña incorrectos" });
-    }
-  } catch (err) {
-    if (err.name === "ValidationError") {
-      res.status(422).json({ errors: err.errors });
-    } else {
-      next(err);
-    }
-  }
-});
-
 
 // Manejo de errores
 app.use((err, req, res, next) => {
